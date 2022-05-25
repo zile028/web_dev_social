624e2c7f8d47cdb9e875aae7f687353929b4ce0e
Dejan Zivkovic <zile028@gmail.com>
2022-05-25 09:16:13 +0200

    Update Connection.php

--- core/classes/Connection.php
+++ core/classes/Connection.php
@@ -1,18 +1,1 @@
-<?php
-
-class Connection
-{
-    public function __construct($host, $dbname, $username, $password)
-    {
-        try {
-            $this->db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        } catch (PDOException $err) {
-            echo $err->getMessage();
-            die();
-        }
-    }
-}
-
-$Connection = new Connection($cfg["db"]["host"], $cfg["db"]["dbname"], $cfg["db"]["username"], $cfg["db"]["password"]);
-
 
