6df3adf02943890bba8630bf70e4845ccb7c4ede
zile028 <zile028@gmail.com>
2022-05-28 01:58:33 +0200

    voting

--- core/classes/Posts.php
+++ core/classes/Posts.php
@@ -5,10 +5,12 @@ class Posts extends QueryBuilder
     public function getAllPosts()
     {
         $sql = "SELECT 
-                p.* , u.first_name, u.last_name
+                p.* , u.first_name, u.last_name, SUM(ld.likes) AS likes, SUM(ld.dislike) AS dislike
                 FROM posts as p
                 JOIN users as u ON u.id=p.user_id
+                LEFT JOIN likes_dislike ld on p.id = ld.post_id
                 WHERE visibility = :visibility OR p.user_id = :user_id
+                GROUP BY p.id
                 ";
 
         $query = $this->db->prepare($sql);
@@ -22,8 +24,7 @@ class Posts extends QueryBuilder
 
     public function addPost(array $data)
     {
-        $sql = "INSERT INTO posts 
-                (title, post_text, img, visibility, user_id)
+        $sql = "INSERT INTO posts (title, post_text, img, visibility, user_id)
                 VALUES (:title, :post_text, :img, :visibility, :user_id)";
         $query = $this->db->prepare($sql);
         $query->execute($data);
@@ -44,22 +45,55 @@ class Posts extends QueryBuilder
 
     }
 
+    private function isVoted($user_id, $post_id)
+    {
+        $sql = "SELECT * FROM likes_dislike WHERE post_id=:post_id AND user_id = :user_id";
+        $query = $this->db->prepare($sql);
+        $query->execute([
+            "user_id" => $user_id,
+            "post_id" => $post_id
+        ]);
+        return $query->fetch(PDO::FETCH_OBJ);
+    }
+
     public function like($post_id)
     {
-        $post_likes = $this->singlePost($post_id)->likes;
-        $post_likes++;
-        $sql = "UPDATE posts SET likes = :likes WHERE id = :post_id";
+        $is_voted = $this->isVoted($_SESSION["id"], $post_id);
+        $likes = $is_voted->likes;
+        $dislike = $is_voted->dislike;
+
+        if ($is_voted) {
+            $sql = "UPDATE likes_dislike SET dislike = :dislike, likes=:likes WHERE post_id = :post_id AND user_id = :user_id";
+            $likes = $likes === 0 ? 1 : 0;
+            $dislike = $likes === 1 ? 0 : $dislike;
+        } else {
+            $sql = "INSERT INTO likes_dislike (post_id, user_id, dislike, likes) VALUES (:post_id, :user_id, :dislike, :likes)";
+            $dislike = 0;
+            $likes = 1;
+        }
         $query = $this->db->prepare($sql);
-        $query->execute(["post_id" => $post_id, "likes" => $post_likes]);
+        $query->execute(["post_id" => $post_id, "dislike" => $dislike, "likes" => $likes, "user_id" => $_SESSION["id"]]);
+
     }
 
     public function dislike($post_id)
     {
-        $post_dislikes = $this->singlePost($post_id)->dislike;
-        $post_dislikes++;
-        $sql = "UPDATE posts SET dislike = :dislikes WHERE id = :post_id";
+        $is_voted = $this->isVoted($_SESSION["id"], $post_id);
+        $likes = $is_voted->likes;
+        $dislike = $is_voted->dislike;
+
+        if ($is_voted) {
+            $sql = "UPDATE likes_dislike SET dislike = :dislike, likes=:likes WHERE post_id = :post_id AND user_id = :user_id";
+            $dislike = $dislike === 0 ? 1 : 0;
+            $likes = $dislike === 1 ? 0 : $likes;
+        } else {
+            $sql = "INSERT INTO likes_dislike (post_id, user_id, dislike, likes) VALUES (:post_id, :user_id, :dislike, :likes)";
+            $dislike = 1;
+            $likes = 0;
+        }
         $query = $this->db->prepare($sql);
-        $query->execute(["post_id" => $post_id, "dislikes" => $post_dislikes]);
+        $query->execute(["post_id" => $post_id, "dislike" => $dislike, "likes" => $likes, "user_id" => $_SESSION["id"]]);
+
     }
 }
 
